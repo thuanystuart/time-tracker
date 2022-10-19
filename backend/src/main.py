7d7d52e223abcaafc02a9598b5208020686e3b56
thuanystuart <thuanystuart@gmail.com>
2022-10-19 14:56:53 +0200

    backend: adds migrations to the db

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 
-from entities.db import db
+from src.entities.db import db, migrate
 
 load_dotenv()
 
@@ -20,12 +20,13 @@ app.app_context().push()
 cors = CORS(app, supports_credentials=True)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 metadata = db.metadata
 metadata.reflect(bind=db.engine)
+migrate.init_app(app, db)
 
 with app.app_context():
     db.create_all()
