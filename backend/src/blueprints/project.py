acc97f1adfcae0c8787856a0fa668d459dd82df1
thuanystuart <thuanystuart@gmail.com>
2022-10-20 12:51:15 +0200

    backend: creates api endpoints for projects

--- backend/src/blueprints/project.py
+++ backend/src/blueprints/project.py
@@ -4,30 +4,25 @@ from flask_cors import CORS
 from src.entities.utils import db
 from src.entities.project import Project
 from src.serializers.project import ProjectSchema
+from src.blueprints.utils import RequestManager
 
 project_page = Blueprint('project_page', __name__)
 CORS(project_page, supports_credentials=True)
 
+request_manager = RequestManager(Project, ProjectSchema, 'project')
+
 @project_page.route('/project', methods=['GET'])
 def get_projects():
-    projects = Project.query.all()
-    return ProjectSchema().dump(projects, many=True)
+    return request_manager.get()
 
 @project_page.route('/project', methods=['POST'])
 def add_project():
-    name = request.json.get('name')
+    return request_manager.add(request)
 
-    new_project = Project(
-      name=name,
-      )
-
-    db.session.add(new_project)
-    db.session.commit()
-    return ProjectSchema().dump(new_project)
+@project_page.route('/project', methods=['PUT'])
+def update_project():
+    return request_manager.update(request)
 
 @project_page.route('/project', methods=['DELETE'])
 def delete_project():
-    id = request.args.get('id')
-    Project.query.filter_by(id=id).delete()
-    db.session.commit()
-    return make_response("The project was successfully deleted.", 200)
+    return request_manager.delete(request)
--- backend/src/serializers/project.py
+++ backend/src/serializers/project.py
@@ -2,10 +2,9 @@ from src.serializers.utils import ma
 from src.entities.project import Project
 from marshmallow import fields
 
-class ProjectSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Project
+class ProjectSchema(ma.SQLAlchemyAutoSchema):
+  class Meta:
+    model = Project
+    load_instance = True
 
-    id = ma.auto_field()
-    name = ma.auto_field()
-    tasks = fields.Nested('TaskSchema', many=True, exclude=["project"])
+  tasks = fields.Nested('TaskSchema', many=True, exclude=["project"], dump_only=True)
