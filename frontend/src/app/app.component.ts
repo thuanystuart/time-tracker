aa0ba0a245fe10b8a945ea83d8bdb3a28b5ae2d0
thuanystuart <thuanystuart@gmail.com>
2022-11-02 19:05:03 +0100

    frontend: add loading page while app initializes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ApplicationInitStatus, APP_INITIALIZER, Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(@Inject(APP_INITIALIZER) public appInit: ApplicationInitStatus) { }
 }
