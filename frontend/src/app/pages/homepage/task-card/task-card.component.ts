242752cc04c96a593434ddd2d3649e22a7665928
thuanystuart <thuanystuart@gmail.com>
2022-11-06 11:21:42 +0100

    frontend: add time span and duration to task-card

--- frontend/src/app/pages/homepage/task-card/task-card.component.ts
+++ frontend/src/app/pages/homepage/task-card/task-card.component.ts
@@ -1,14 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Task } from '@entities/task.model';
+import { DateTime, Duration } from 'luxon';
 
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
   styleUrls: ['./task-card.component.scss']
 })
-export class TaskCardComponent {
+export class TaskCardComponent implements OnInit {
   @Input() task : Task | undefined
   @Output() deleteTask = new EventEmitter<number>()
+
+  duration = Duration.fromMillis(0)
+  start_datetime: DateTime = DateTime.now()
+  end_datetime: DateTime = DateTime.now()
+
+  ngOnInit(): void {
+    if (this.task && this.task.start_datetime && this.task.end_datetime) {
+      this.end_datetime = DateTime.fromISO(this.task.end_datetime)
+      this.start_datetime = DateTime.fromISO(this.task.start_datetime)
+      this.duration = this.end_datetime.diff(this.start_datetime)
+    }
+  }
 
   onDeleteTask() {
     this.deleteTask.emit(this.task?.id)
